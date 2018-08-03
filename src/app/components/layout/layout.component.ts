e7adbed4ff91c476863f485824647bd5d49b706f
Guy-Kaplan <39667435+Guy-Kaplan@users.noreply.github.com>
2018-08-03 11:14:05 +0300

    Update layout

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -12,6 +12,18 @@ export class LayoutComponent implements OnInit {
 
     ngOnInit() {
         this.t.setTitle("Web assignment");
+        window.onscroll = function () { myFunction() };
+
+        var header = document.getElementById("header");
+        var sticky = header.offsetTop;
+
+        function myFunction() {
+            if (window.pageYOffset > sticky) {
+                header.classList.add("sticky");
+            } else {
+                header.classList.remove("sticky");
+            }
+        }
     }
 
 }
